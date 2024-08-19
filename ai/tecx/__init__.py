f5f255f54f32feb9c6b22eb68ec27a011ebdff88
TecX-Adm <adm.tecx7@gmail.com>
2024-08-19 12:08:56 +0530

    Add files via upload

---
+++ ai/tecx/__init__.py
@@ -0,0 +1,17 @@
+__author__ = "TecX's C.T.O. RAHUL SAINI"
+__maintainer__ = __author__
+
+import ai.tecx.sso.signintoai as siai
+import ai.tecx.sso.signuptoai as suai
+
+#from .signintoai import SignIntoAI as sitai
+#from .signuptoai import SignUptoAI as sutai
+#import signintoai as Siai
+#import signuptoai as suai
+#from . import aitecx
+
+__all__ = [
+    'SignIntoAI'
+    'SignUptoAI'
+   # 'AITecX'
+]
---
+++ ai/tecx/admin/aitecxadmin.py
@@ -0,0 +1,5 @@
+__author__ = "TecX's C.T.O. RAHUL SAINI"
+__maintainer__ = __author__
+
+class AITecXAdmin:
+    print("This is a TecXAdmin Class, Which is in the ai.tecx.admin Package.")
